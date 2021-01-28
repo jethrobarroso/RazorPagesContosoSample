95b51fa82335e5040e0448d7501e206cffdcad39
Jethro <jethrobar@gmail.com>
2021-01-28 09:19:28 +0200

    Add project files.

---
+++ RazorPagesContoso/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesContoso.Data;
+using RazorPagesContoso.Models;
+using RazorPagesContoso.ViewModels;
+
+namespace RazorPagesContoso.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesContoso.Data.SchoolContext _context;
+
+        public CreateModel(RazorPagesContoso.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public StudentVM StudentVM { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var entry = _context.Add(new Student());
+
+            // This will try to match values from the StudentVM and apply it to the entity's properties
+            entry.CurrentValues.SetValues(StudentVM);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesContoso/Pages/Students/Edit.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesContoso.Data;
+using RazorPagesContoso.Models;
+
+namespace RazorPagesContoso.Pages.Students
+{
+    public class EditModel : PageModel
+    {
+        private readonly RazorPagesContoso.Data.SchoolContext _context;
+
+        public EditModel(RazorPagesContoso.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students.FindAsync(id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var studentToUpdate = await _context.Students.FindAsync(id);
+
+            if (studentToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            // Aside from using Viewmodel to prevent overposting, you can use TryUpdateModel 
+            // to choose which properties are bound to the form
+            if (await TryUpdateModelAsync<Student>(
+                studentToUpdate,
+                nameof(Student), // Searches the prefix on the name attribute eg. Student.LastName
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
